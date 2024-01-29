bf93e16fb577b0d4622d48ea40dafb1dd46b497a
Maxim Yugay <sayamonkoh@gmail.com>
2024-01-30 01:18:48 +0300

    add fileBase routing

--- server/index.js
+++ server/index.js
@@ -14,8 +14,24 @@ const myScript = fs.readFileSync(
 
 const scriptPathRegexp = /.*\.js/;
 
-app.get('/', (req, res) => {
-  res.send(myHtml);
+const myPages = fs
+  .readdirSync(path.resolve(__dirname, '..', 'src'))
+  .filter((page) => page !== 'App.tsx');
+
+myPages.forEach(async (page) => {
+  // const pageFunction = fs.readFileSync(
+  //   path.resolve(__dirname, '..', 'src', page),
+  //   'utf-8'
+  // );
+  const pageImport = await import(path.join(__dirname, '..', 'src', page))
+    .default;
+
+  const pageRouteName = page.replace('.tsx', '');
+  const pageRoute = pageRouteName === 'index' ? '/' : `/${pageRouteName}`;
+
+  app.get(pageRoute, (req, res) => {
+    res.send(myHtml);
+  });
 });
 
 app.get(scriptPathRegexp, (req, res) => {
@@ -24,4 +40,9 @@ app.get(scriptPathRegexp, (req, res) => {
 
 app.listen(4001, () => {
   console.log('Server is running on port 4001');
+  // require('child_process').exec(
+  //   process.platform.replace('darwin', '').replace(/win32|linux/, 'xdg-') +
+  //     'open ' +
+  //     'http://localhost:4001'
+  // );
 });
